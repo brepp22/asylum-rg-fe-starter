b0cf3ec8aa55947f14b3d9bd0b8959e72610f835
brepp22 <brepp@hotmail.com>
2024-09-21 14:45:53 -0700

    style added to bottom

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -8,7 +8,7 @@ import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -18,6 +18,7 @@ function RenderLandingPage(props) {
 
   const history = useHistory();
 
+  //put images in variables for use 
   const barGraph = GrantRatesByOfficeImg;
   const pieChart = GrantRatesByNationalityImg;
   const lineGraph = GrantRatesOverTimeImg;
@@ -91,7 +92,31 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+           <h1>Systemic Disparity Insights</h1> 
+           <div class="data-container">
+              <div class="first-data-point-container">
+                <h2>36%</h2>
+                <h3>By the end of the Trump administration, 
+                  the average asylum office grant rate had 
+                  fallen 36 percent from an average of 44 
+                  percent in fiscal year 2016 to 28 percent 
+                  in fiscal year 2020.</h3>
+              </div>
+              <div class="second-data-point-container">
+                <h2>5%</h2>
+                <h3>The New York asylum office grant rate 
+                  dropped to 5 percent in fiscal year 2020.</h3>
+              </div>
+              <div class="third-data-point-container">
+                <h2>6x Lower</h2>
+                <h3>Between fiscal year 2017 and 2020, 
+                  the New York asylum office’s average grant 
+                  rate was six times lower than the San 
+                  Francisco asylum office.</h3>
+              </div>
+           </div>
+          </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
