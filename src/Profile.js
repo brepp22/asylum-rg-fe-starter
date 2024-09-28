6ad0d2d4d023acc75af94dca40d563fb2d5fa868
brepp22 <brepp@hotmail.com>
2024-09-28 11:50:29 -0700

    update to profile page

--- src/Profile.js
+++ src/Profile.js
@@ -12,12 +12,21 @@ function Profile() {
 
   return (
     isAuthenticated && (
-      <Card style={{ width: 300, margin: 'auto', marginTop: '20px' }}>
+      <div style={{ display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: '100vh', 
+        textAlign: 'center',
+        paddingTop: '20px'}}>
+        <h1>Welcome {user.name} to Your Profile!</h1>
+      <Card style={{ width: 300, margin: 'auto', marginTop: '20px', marginBottom: '10px'}}>
         <h2>Profile</h2>
         <img src={user.picture} alt={user.name} style={{ borderRadius: '50%', width: 100 }} />
         <h3>{user.name}</h3>
         <p>{user.email}</p>
       </Card>
+      </div>
     )
   );
 }
